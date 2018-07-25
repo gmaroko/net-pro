9c7590fe2a2b1781866cd79011e39b540c12c4de
unknown <marokogideon@gmail.com>
2018-07-25 13:39:04 +0300

    Added configure_values, resolve for dif value conflicts

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+Start of script
+Act as a helper to resolve conflict of different values for different classes: eg octets!
+"""
+
+def configure_values(a_class):
+    clasA, clasB, clsC = {}, {}, {} #store them as dict values
+
+    #class A
+    clsA['octet_for_subnet_mask'] = 1 #index == 2nd octet
+    clsA['dedicated_bits'] = 24
+    clsA['default_mask'] = ['255', '0', '0', '0'] #255.0.0.0
+
+    #class B
+    clsB['octet_for_subnet_mask'] = 3 #index == 3rd octet
+    clsB['dedicated_bits'] = 16
+    clsB['default_mask'] = ['255', '255', '0', '0'] #255.0.0.0
+
+    #class C
+    clsC['octet_for_subnet_mask'] = 1 #index == 2nd octet
+    clsC['dedicated_bits'] = 8
+    clsC['default_mask'] = ['255', '255', '255', '0'] #255.0.0.0
+
+    #avoid return all values to ease processing later
+    if a_class == 'A' or 1:
+        return clsA
+    elif a_class == 'B' or 2:
+        return clsB
+    elif a_class == 'C' or 3:
+        return clsC
+    else:
+        print("Unknow class!")
