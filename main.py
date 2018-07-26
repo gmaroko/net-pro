ef22fa9de924612d2a4adefd4eb0c0913521c6b3
unknown <marokogideon@gmail.com>
2018-07-26 22:05:41 +0300

    moved main func from subnetB.py to main.py

--- main.py
+++ main.py
@@ -40,9 +40,38 @@ def main(): #intending to use argparse
     parser.add_argument("netaddres", help="The network address to be subnetted!")
     parser.add_argument("subnets", help="Number of desired subnets!") #must accomodate the available - for future
     parser.add_argument("-c", "--class", help="IP Class", choices=['A','B','C'])
-    inputs = parser.parse_args()
+    net_details = parser.parse_args()
 
-    print(inputs)
+    #num_available_nodes = 2**(DEDICATED_BITS-NUM_OF_BITS_STOLEN) - 2
+    DEFAULT_MASK, DEDICATED_BITS, SUBNET_OCTET = default_value()
+    #calling non func step by step as prev called outside functions to solve import issues
+    #user details
+    NET_ADDRESS, NUM_OF_SUBNETS = get()
+    #bits stolen
+    NUM_OF_BITS_STOLEN = bits_stolen(NUM_OF_SUBNETS)
+    #global for IP ranges
+    LOWEST_OF_HIGH_ORDER_BITS = min(BIT_ORDER[:NUM_OF_BITS_STOLEN])
+    #print(LOWEST_OF_HIGH_ORDER_BITS)
+    find_ranges(NET_ADDRESS, NUM_OF_SUBNETS, LOWEST_OF_HIGH_ORDER_BITS)
+
+    range_ips = load_and_dump()
+    if '\n' in range_ips:
+        range_ips.remove('\n')
+
+
+    cleaned_start_end = clean(range_ips, LOWEST_OF_HIGH_ORDER_BITS)
+    num_available_nodes = 2**(DEDICATED_BITS-NUM_OF_BITS_STOLEN) - 2
+
+    print_out(cleaned_start_end)
+    print("===============================================")
+
+    print("\n\n")
+    print("Network address: %s\n"%('.'.join(NET_ADDRESS)))
+    print("New subnet mask: %s\n"%(sub_net_mask(DEFAULT_MASK, NUM_OF_BITS_STOLEN, SUBNET_OCTET)))
+    print("Number of subnets: %d\n"%(NUM_OF_SUBNETS))
+    print("Available node addresses: %d\n"%(num_available_nodes))
+    print()
+
 
 
 
